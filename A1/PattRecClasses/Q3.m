24073363917a225013c6aaa99deded42db339c35
physics <3150105638@zju.edu.cn>
2020-04-14 23:04:46 +0200

    update

--- A1/PattRecClasses/Q3.m
+++ A1/PattRecClasses/Q3.m
@@ -3,6 +3,15 @@ b1 = GaussD('Mean', 0, 'StDev', 1);
 b2 = GaussD('Mean', 3, 'StDev', 2);
 h = HMM(mc, [b1, b2]);
 
-[X,S]=rand(h,T);
-mu = mean(X)
-va = var(X)
+n=10;
+mu=zeros(1,n);
+va=zeros(1,n);
+for i = 1:n
+    [X,S]=rand(h,T);
+    mu(i) = mean(X);
+    va(i) = var(X);
+end
+
+MU=mean(mu)
+VA=mean(va)
+
